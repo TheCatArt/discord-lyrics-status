4fb73e9044186b051c5006f7342fda48c043d141
The_Cat_Art <kalarko260@gmail.com>
2024-08-14 12:43:48 +0200

    Create discord.py

---
+++ discord.py
@@ -0,0 +1,20 @@
+import discord
+import asyncio
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
+
+    while True:
+        track_name, artist_name = get_current_playing()
+        if track_name and artist_name:
+            lyrics = get_lyrics(track_name, artist_name)
+            if lyrics:
+                lines = lyrics.split('\n')
+                for line in lines:
+                    await client.change_presence(activity=discord.Game(name=line))
+                    await asyncio.sleep(10)
+                await asyncio.sleep(5)
+client.run('DISCORD_TOKEN')
