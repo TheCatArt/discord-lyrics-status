43b0ebf1e779d32296a6b367b7475629b8712866
The_Cat_Art <kalarko260@gmail.com>
2024-08-14 12:44:34 +0200

    Delete get_lyrics.py

--- get_lyrics.py
+++
@@ -1,7 +0,0 @@
-import lyricsgenius
-
-genius = lyricsgenius.Genius("ACCESS_TOKEN")
-
-def get_lyrics(song_title, artist_name):
-    song = genius.search_song(song_title, artist_name)
-    return song.lyrics if song else None
